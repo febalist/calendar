6f992f41c2c49297ae974a4533065b07012c187c
febalist <febalist@gmail.com>
2018-10-30 23:10:49 +0300

    Addition and Subtraction workdays

--- src/Calendar.php
+++ src/Calendar.php
@@ -32,6 +32,34 @@ class Calendar extends Carbon
         return $this->inCalendar('preholidays');
     }
 
+    public function addWorkday($value = 1)
+    {
+        return $this->addWorkdays($value);
+    }
+
+    public function addWorkdays($value)
+    {
+        $workdays = 0;
+        $step = $value >= 0 ? 1 : -1;
+
+        while ($workdays < $value) {
+            $this->addDay($step);
+            $workdays += $this->isWorkday();
+        }
+
+        return $this;
+    }
+
+    public function subWorkday($value = 1)
+    {
+        return $this->subWorkdays($value);
+    }
+
+    public function subWorkdays($value)
+    {
+        return $this->addWorkdays(-1 * $value);
+    }
+
     protected function inCalendar($type)
     {
         return in_array($this->toDateString(), static::calendar()[$type]);
