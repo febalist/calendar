40b2fb8c00396813a18506436f509f65ca5bcdd2
febalist <febalist@gmail.com>
2020-11-06 10:18:22 +0300

    .env

--- load.sh
+++ load.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-token=$1
-[[ -n $token ]] || exit 1
+source .env
+
+[[ -n $TOKEN ]] || exit 1
 
 url="https://data.gov.ru/api/json/dataset/7708660670-proizvcalendar/version/20151123T183036/content"
-wget -O data/content.json "$url?access_token=$token"
+wget -O data/content.json "$url?access_token=$TOKEN"
