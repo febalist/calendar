686fa6684bff7592499e95fbd65f6d7238409866
febalist <febalist@gmail.com>
2020-12-23 13:29:03 +0300

    timeout

--- load.sh
+++ load.sh
@@ -4,4 +4,4 @@ source .env
 [[ -n $TOKEN ]] || exit 1
 
 url="https://data.gov.ru/api/json/dataset/7708660670-proizvcalendar/version/20151123T183036/content"
-wget -O data/content.json "$url?access_token=$TOKEN"
+wget --timeout 10 -O data/content.json "$url?access_token=$TOKEN"
