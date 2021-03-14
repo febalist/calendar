ee28c9ef888e3cd8d1603330dc272a211d10f69d
febalist <febalist@gmail.com>
2021-03-14 16:26:44 +0300

    New source

--- generate.php
+++ generate.php
@@ -2,38 +2,41 @@
 
 namespace Febalist\Calendar;
 
-$major = json_decode(file_get_contents(__DIR__.'/data/major.json'), true);
-$content = json_decode(file_get_contents(__DIR__.'/data/content.json'), true);
+use Carbon\Carbon;
+
+require __DIR__.'/vendor/autoload.php';
+
+$data = fopen(__DIR__.'/data/data.txt', 'rb');
+$major = yaml_parse_file(__DIR__.'/data/major.yaml');
 $csv = fopen(__DIR__.'/src/data.csv', 'wb');
-$data = [];
-$year_major = [];
+$json = [];
 
-foreach ($content as $year_content) {
-    $year_content = array_values($year_content);
-    $year = (int) $year_content[0];
+foreach (range(2004, 2025) as $year) {
+    $values = str_split(fgets($data));
+    $date = new Carbon("$year-01-01");
+    while ($date->year === $year) {
+        $value = array_shift($values);
+        $month = $date->month;
+        $day = $date->day;
 
-    if (isset($major[$year])) {
-        $year_major = $major[$year];
-    }
-
-    foreach (range(1, 12) as $month) {
-        $days = explode(',', $year_content[$month]);
-        foreach ($days as $day_data) {
-            $day = (int) $day_data;
-
-            if (in_array($day, $year_major[$month] ?? [])) {
-                $type = 3;
-            } elseif (strpos($day_data, '*') !== false) {
-                $type = 1;
-            } else {
-                $type = 2;
-            }
-
-            fputcsv($csv, [date_create("$year-$month-$day")->format("Y-m-d"), $type]);
-            $data[$year][$month][$day] = $type;
+        if (in_array($day, $major[$year][$month] ?? [])) {
+            $type = 3;
+        } elseif ($value === '2') {
+            $type = 1;
+        } elseif ($value === '1') {
+            $type = 2;
+        } else {
+            $type = 0;
         }
+
+        if ($type) {
+            fputcsv($csv, [date_create("$year-$month-$day")->format("Y-m-d"), $type]);
+            $json[$year][$month][$day] = $type;
+        }
+
+        $date->addDay();
     }
 }
 
 fclose($csv);
-file_put_contents(__DIR__.'/src/data.json', json_encode($data));
+file_put_contents(__DIR__.'/src/data.json', json_encode($json));
